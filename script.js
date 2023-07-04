e49da80de8e9428dc0f668bec1aff8f4697020a9
Peter Lauer <lauerpeter11@gmail.com>
2023-07-04 15:15:57 -0500

    optimized prompts stuck on if statements

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var characterUpper
 var characterSpecial
 var characterNumeric
 
-var characterLength = [8-128]
+var characterLength = [("8-128")]
 var characterLower = ["a", "b", "c", "d", "e", "f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] ;
 var characterUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] ;
 var characterSpecial = ["!", "@", ")", "$", "%", "^", "&", "*", "(", "#"] ;
@@ -27,20 +27,19 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  characterLength = prompt ("choose a length of at least 8 characters and no more than 128 characters for your password")
-  characterLower = prompt ("Would you like to include lower case characters in your password?")
-  characterUpper = prompt ("Would you like to include upper case characters in your password? ")
-  characterSpecial = prompt ("Would you like to iclude special characters in your password?")
-  characterNumeric = prompt ("Would yopu like to include numbers in your password?")
+  
+  characterLength = prompt ("Please choose a password length of at least 8 characters and no more than 128 characters");
+
+  characterLower = prompt ("Would you like to include lower case characters in your password? Please type yes or no");
+ 
+  characterUpper = prompt ("Would you like to include upper case characters in your password? Please type yes or no");
+  
+  characterSpecial = prompt ("Would you like to iclude special characters in your password? Please type yes or no");
+  
+  characterNumeric = prompt ("Would yopu like to include numbers in your password? Please type yes or no");
+
 }
 
-
-
- 
-
-
-
-
-
-
+  
+  
 
